0bf080f4feae7bb0087ef3e273a12cbe2954fae7
Clear008 <wissal-exo@hotmail.com>
2024-03-17 14:39:38 +0000

    setup_mysql_test.sql

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,3 +1,7 @@
+-- Prepares a MySQL server for the project
+-- create database hbnb_dev_db and user 
+-- Grant privileges
+
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
@@ -5,4 +9,3 @@ CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
 
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
-
