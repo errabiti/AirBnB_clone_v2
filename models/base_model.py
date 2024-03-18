fea8af9cdd4b5e05e4a83df50b5cac3837e4e5a4
Clear008 <wissal-exo@hotmail.com>
2024-03-18 15:12:27 +0000

    base

--- models/base_model.py
+++ models/base_model.py
@@ -22,12 +22,16 @@ class BaseModel:
             for key, value in kwargs.items():
                 if key != "__class__":
                     setattr(self, key, value)
-            if kwargs.get("created_at", None) and isinstance(kwargs["created_at"], str):
-                self.created_at = datetime.strptime(kwargs["created_at"], '%Y-%m-%dT%H:%M:%S.%f')
+            if (kwargs.get("created_at", None) and
+                    isinstance(kwargs["created_at"], str)):
+                self.created_at = datetime.strptime(
+                    kwargs["created_at"], '%Y-%m-%dT%H:%M:%S.%f')
             else:
                 self.created_at = datetime.utcnow()
-            if kwargs.get("updated_at", None) and isinstance(kwargs["updated_at"], str):
-                self.updated_at = datetime.strptime(kwargs["updated_at"], '%Y-%m-%dT%H:%M:%S.%f')
+            if (kwargs.get("updated_at", None) and
+                    isinstance(kwargs["updated_at"], str)):
+                self.updated_at = datetime.strptime(
+                    kwargs["updated_at"], '%Y-%m-%dT%H:%M:%S.%f')
             else:
                 self.updated_at = datetime.utcnow()
             if kwargs.get("id", None) is None:
