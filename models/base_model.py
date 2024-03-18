60017a09f6bf8bdf3968d3b112891f10dffcf83e
Clear008 <wissal-exo@hotmail.com>
2024-03-18 12:10:13 +0000

    init function correction

--- models/base_model.py
+++ models/base_model.py
@@ -18,33 +18,26 @@ class BaseModel:
     updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
 
     def __init__(self, *args, **kwargs):
-        """Instatntiates a new model"""
-        if 'id' not in kwargs:
-            kwargs['id'] = str(uuid.uuid4())
-        if not kwargs:
+        """Initialization of the base model"""
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    setattr(self, key, value)
+            if kwargs.get("created_at", None) and isinstance(kwargs["created_at"], str):
+                self.created_at = datetime.strptime(kwargs["created_at"], '%Y-%m-%dT%H:%M:%S.%f')
+            else:
+                self.created_at = datetime.utcnow()
+            if kwargs.get("updated_at", None) and isinstance(kwargs["updated_at"], str):
+                self.updated_at = datetime.strptime(kwargs["updated_at"], '%Y-%m-%dT%H:%M:%S.%f')
+            else:
+                self.updated_at = datetime.utcnow()
+            if kwargs.get("id", None) is None:
+                self.id = str(uuid.uuid4())
+        else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.utcnow()
-            self.updated_at = datetime.utcnow()
-        else:
-            if 'updated_at' not in kwargs:
-                kwargs['updated_at'] = kwargs.get
-                ('created_at', datetime.utcnow())
-
-            if 'created_at' not in kwargs:
-                kwargs['created_at'] = datetime.utcnow()
-
-            if isinstance(kwargs.get('updated_at'), str):
-                kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                '%Y-%m-%dT%H:%M:%S.%f')
-
-            if isinstance(kwargs.get('created_at'), str):
-                kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                        '%Y-%m-%dT%H:%M:%S.%f')
-
-            kwargs.pop('__class__', None)
-            self.__dict__.update(kwargs)
-        super().__init__(*args, **kwargs)
-
+            self.updated_at = self.created_at
+            
     def __str__(self):
         """Returns a string representation of the instance"""
         cls = (str(type(self)).split('.')[-1]).split('\'')[0]
