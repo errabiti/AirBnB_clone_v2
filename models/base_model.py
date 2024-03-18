be788327d3f5057efc776f1fe0430e4cdd9aa054
Clear008 <wissal-exo@hotmail.com>
2024-03-18 03:38:37 +0000

    fixe some errors

--- models/base_model.py
+++ models/base_model.py
@@ -3,11 +3,19 @@
 import uuid
 from datetime import datetime
 from sqlalchemy.ext.declarative import declarative_base
+import models
+from sqlalchemy import Column, String, DateTime
 
 Base = declarative_base()
 
+
 class BaseModel:
     """A base class for all HBNB models"""
+
+    id = Column(String(60), nullable=False, primary_key=True)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
@@ -18,20 +26,21 @@ class BaseModel:
             storage.new(self)
         else:
             if 'updated_at' not in kwargs:
-                kwargs['updated_at'] = kwargs.get('created_at', datetime.utcnow())
+                kwargs['updated_at'] = kwargs.get
+                ('created_at', datetime.utcnow())
 
             if 'created_at' not in kwargs:
                 kwargs['created_at'] = datetime.utcnow()
 
             if isinstance(kwargs.get('updated_at'), str):
                 kwargs['updated_at'] = datetime.strptime(kwargs['updated_at'],
-                                                         '%Y-%m-%dT%H:%M:%S.%f')
+                                                '%Y-%m-%dT%H:%M:%S.%f')
 
             if isinstance(kwargs.get('created_at'), str):
                 kwargs['created_at'] = datetime.strptime(kwargs['created_at'],
-                                                         '%Y-%m-%dT%H:%M:%S.%f')
+                                        '%Y-%m-%dT%H:%M:%S.%f')
 
-            kwargs.pop('__class__', None)  # Use pop to avoid KeyError
+            kwargs.pop('__class__', None)
             self.__dict__.update(kwargs)
 
     def __str__(self):
@@ -42,14 +51,19 @@ class BaseModel:
     def save(self):
         """Updates updated_at with current time when instance is changed"""
         from models import storage
-        self.updated_at = datetime.utcnow()  # Use datetime.utcnow() to get current UTC time
+        self.updated_at = datetime.utcnow()
         storage.save()
 
     def to_dict(self):
         """Convert instance into dict format"""
         dictionary = {}
         dictionary.update(self.__dict__)
-        dictionary.update({'__class__': (str(type(self)).split('.')[-1]).split('\'')[0]})
+        dictionary.update({'__class__':
+                           (str(type(self)).split('.')[-1]).split('\'')[0]})
         dictionary['created_at'] = self.created_at.isoformat()
         dictionary['updated_at'] = self.updated_at.isoformat()
         return dictionary
+
+    def delete(self):
+        """Deletes the current instance from the storage"""
+        models.storage.delete()
--- models/city.py
+++ models/city.py
@@ -2,10 +2,11 @@ from sqlalchemy import Column, String, ForeignKey
 from sqlalchemy.orm import relationship
 from models.base_model import BaseModel, Base
 
+
 class City(BaseModel, Base):
     """ The city class, contains state ID and name """
     __tablename__ = "cities"
 
-    id = Column(String(60), primary_key=True)  # Primary key column for the 'cities' table
     state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
     name = Column(String(128), nullable=False)
+    places = relationship("Place", backref="cities", cascade="delete")
