ec7f16457bd34dfac7d3eea20ad97b6cbb863c02
errabiti <errabitibadr@gmail.com>
2024-04-06 01:18:09 +0200

    web static

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -1,18 +1,22 @@
 #!/usr/bin/python3
-""" Generates an archive (.tgz) from the contents of the web_static folder """
-from fabric.api import local
+# Fabfile to generates a .tgz archive from the contents of web_static.
+import os.path
 from datetime import datetime
-import os
+from fabric.api import local
 
 
 def do_pack():
-    """Creates a .tgz archive from the contents of the web_static folder."""
-    try:
-        time = datetime.now().strftime("%Y%m%d%H%M%S")
-        if not os.path.exists("versions"):
-            local("mkdir versions")
-        archive_p = 'versions/web_static_{}.tgz'.format(time)
-        local("tar -czvf {} web_static".format(archive_p))
-        return archive_p
-    except FileNotFoundError:
+    """Create a tar gzipped archive of the directory web_static."""
+    dt = datetime.utcnow()
+    file = "versions/web_static_{}{}{}{}{}{}.tgz".format(dt.year,
+                                                         dt.month,
+                                                         dt.day,
+                                                         dt.hour,
+                                                         dt.minute,
+                                                         dt.second)
+    if os.path.isdir("versions") is False:
+        if local("mkdir -p versions").failed is True:
+            return None
+    if local("tar -cvzf {} web_static".format(file)).failed is True:
         return None
+    return file
--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -1,22 +1,30 @@
 #!/usr/bin/python3
-""" that deletes out-of-date archives, using the function do_clean """
+# Fabfile to delete out-of-date archives.
+import os
 from fabric.api import *
 
-
-env.hosts = ['18.206.208.113', '18.206.232.93']
-env.user = "ubuntu"
+env.hosts = ["104.196.168.90", "35.196.46.172"]
 
 
 def do_clean(number=0):
-    """ that deletes out-of-date archives, using the function do_clean """
+    """Delete out-of-date archives.
 
-    number = int(number)
+    Args:
+        number (int): The number of archives to keep.
 
-    if number == 0:
-        number = 2
-    else:
-        number += 1
+    If number is 0 or 1, keeps only the most recent archive. If
+    number is 2, keeps the most and second-most recent archives,
+    etc.
+    """
+    number = 1 if int(number) == 0 else int(number)
 
-    local('cd versions ; ls -t | tail -n +{} | xargs rm -rf'.format(number))
-    path = '/data/web_static/releases'
-    run('cd {} ; ls -t | tail -n +{} | xargs rm -rf'.format(path, number))
+    archives = sorted(os.listdir("versions"))
+    [archives.pop() for i in range(number)]
+    with lcd("versions"):
+        [local("rm ./{}".format(a)) for a in archives]
+
+    with cd("/data/web_static/releases"):
+        archives = run("ls -tr").split()
+        archives = [a for a in archives if "web_static_" in a]
+        [archives.pop() for i in range(number)]
+        [run("rm -rf ./{}".format(a)) for a in archives]
